19b0453ece7eb6669e3eaa6ba0d65a5bbdac3687
RabbitZ <galangputra376@gmail.com>
2022-07-13 19:48:56 +0700

    Pengembalian Stock Ketika Delete

--- admin/mod_penjualan/penjualanCtrl.php
+++ admin/mod_penjualan/penjualanCtrl.php
@@ -12,6 +12,19 @@ if (!isset($_GET['action'])) {
     $dto = mysqli_fetch_array($qjual);
 } else if (isset($_GET['action']) && $_GET['action'] == "delete") {
     $no = $_GET['nojual'];
+    // Query Mengambil Kode_Komik + Stock dari Detail Penjualan
+    $querycek = mysqli_query($koneksidb, "SELECT kode_komik,qty FROM trn_jualdetail WHERE nojual='$no'");
+    foreach ($querycek as $c) {
+        // Perulangan Pengecekan Kode_Komik
+        $kode = $c['kode_komik'];
+        $komik = mysqli_query($koneksidb, "SELECT stock FROM mst_komik WHERE kode_komik='$kode'");
+        $stock = mysqli_fetch_array($komik);
+        // Rumus Penambahan Stock
+        $penambahan = $stock['stock'] + $c['qty'];
+        // Query Penambahan Stock pada mst_komik
+        mysqli_query($koneksidb, "UPDATE mst_komik SET stock='$penambahan' WHERE kode_komik='$kode'");
+    }
+    // Proses Delete Penjualan Dan Detail_Penjualan
     $delete = mysqli_query($koneksidb, "DELETE FROM trn_jualdetail WHERE nojual='$no'");
     if ($delete) {
         mysqli_query($koneksidb, "DELETE FROM trn_jualhead WHERE nojual='$no'");
@@ -35,6 +48,10 @@ if (!isset($_GET['action'])) {
     $qinsert_head = mysqli_query($koneksidb, "UPDATE trn_jualhead SET total='$total' WHERE nojual='$no'") or die("error head" . mysqli_error($koneksidb));
     if ($qinsert_head) {
         for ($i = 0; $i < $jml_list; $i++) {
+            $cekstock = mysqli_query($koneksidb, "SELECT * FROM mst_komik WHERE kode_komik='$kodekomik[$i]'");
+            $cek = mysqli_fetch_array($cekstock);
+            $pengurangan = $cek['stock'] - $qty[$i];
+            $queryupstock = mysqli_query($koneksidb, "UPDATE mst_komik SET stock='$pengurangan' WHERE kode_komik='$kodekomik[$i]'");
             $qinsert_det = mysqli_query($koneksidb, "INSERT INTO trn_jualdetail 
 			(nojual,kode_komik,harga,qty,subtotal) VALUES ('$no_inv', '$kodekomik[$i]', $harga[$i], $qty[$i], $subtotal[$i])")
                 or die("error detail " . mysqli_error($koneksidb));
